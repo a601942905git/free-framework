c3ac58ac78a5d539574db7d7818d1c1c51fa0238
Administrator <a601942905@163.com>
2017-08-03 23:31:24 +0800

    修改日志配置

--- free-framework-admin/src/main/java/com/free/framework/Application.java
+++ free-framework-admin/src/main/java/com/free/framework/Application.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class Application {
     public static void main(String[] args) {
+        log.info("======Spring Boot项目启动开始======");
         SpringApplication.run(Application.class, args);
+        log.info("======Spring Boot项目启动结束======");
     }
 }
