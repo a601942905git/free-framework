0a0e45064d2ddd86c7495a08f673ee8e27c7cb95
Administrator <a601942905@163.com>
2017-08-03 21:05:49 +0800

    修改pom结构

--- free-framework-admin/src/main/java/com/free/framework/Application.java
+++ free-framework-admin/src/main/java/com/free/framework/Application.java
@@ -1,5 +1,6 @@
 package com.free.framework;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Administrator on 2017/6/3.
  */
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- free-framework-admin/src/main/java/com/free/framework/plateform/listener/WebInitListener.java
+++ free-framework-admin/src/main/java/com/free/framework/plateform/listener/WebInitListener.java
@@ -5,24 +5,12 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerInitial
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @Slf4j
 @Component
 public class WebInitListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent>{
 
     @Override
     public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        try {
-            String serverName = event.getApplicationContext().getDisplayName();
-            String serverIp = InetAddress.getLocalHost().getHostAddress();
-            Integer serverPort = event.getEmbeddedServletContainer().getPort();
-            log.info("===============================================================\n\n");
-            log.info("{}应用的服务器地址为:{}", serverName, serverIp + ":" + serverPort);
-            log.info("===============================================================\n\n");
-        } catch (UnknownHostException e) {
-            log.error("【WebInitListener中的onApplicationEvent异常:】", e.fillInStackTrace());
-        }
+
     }
 }
