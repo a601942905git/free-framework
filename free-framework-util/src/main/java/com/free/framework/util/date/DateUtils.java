b6c3b902fa238453e95d5d8e49b8430d87162b2b
lipeng <a601942905>
2017-08-05 09:39:57 +0800

    Add DateUtils Method

--- free-framework-util/src/main/java/com/free/framework/util/date/DateUtils.java
+++ free-framework-util/src/main/java/com/free/framework/util/date/DateUtils.java
@@ -3,9 +3,10 @@ package com.free.framework.util.date;
 import com.free.framework.util.date.constant.FormatterPatternConsts;
 import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
 
 /**
  * Created by Administrator on 2017/6/12.
@@ -54,5 +55,79 @@ public class DateUtils {
     public static DateTimeFormatter getDateTimeFormatter(String formatPattern) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatPattern);
         return dateTimeFormatter;
+    }
+
+    /**
+     * LocalDateTime转Date
+     * @param localDateTime
+     * @return
+     */
+    public static Date convertLocalDateTimeToDate(LocalDateTime localDateTime) {
+        Objects.requireNonNull(localDateTime);
+        Date date;
+        ZoneId zoneId = getSystemZoneId();
+        Instant instant = localDateTime.atZone(zoneId).toInstant();
+        date = Date.from(instant);
+        return date;
+    }
+
+    /**
+     * Date转LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime convertDateToLocalDateTime(Date date) {
+        Objects.requireNonNull(date);
+        Instant instant = date.toInstant();
+        ZoneId zoneId = getSystemZoneId();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
+        return localDateTime;
+    }
+
+    /**
+     * LocalDate转Date
+     * @param localDate
+     * @return
+     */
+    public static Date convertLocalDateToDate(LocalDate localDate) {
+        Objects.requireNonNull(localDate);
+        Date date;
+        ZoneId zoneId = getSystemZoneId();
+        Instant instant = localDate.atStartOfDay(zoneId).toInstant();
+        date = Date.from(instant);
+        return date;
+    }
+
+    /**
+     * Date转LocalDate
+     * @param date
+     * @return
+     */
+    public static LocalDate convertDateToLocalDate(Date date) {
+        Objects.requireNonNull(date);
+        Instant instant = date.toInstant();
+        ZoneId zoneId = getSystemZoneId();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
+        LocalDate localDate = localDateTime.toLocalDate();
+        return localDate;
+    }
+
+    /**
+     * 获取默认时区
+     * @return
+     */
+    public static ZoneId getSystemZoneId() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        return zoneId;
+    }
+
+    /**
+     * 获取系统时间戳
+     * @return
+     */
+    public static Long getSystemMillis() {
+        Clock clock = Clock.systemDefaultZone();
+        Long systemMillis = clock.millis();
+        return systemMillis;
     }
 }
