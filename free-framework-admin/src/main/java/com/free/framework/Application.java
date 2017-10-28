f693edf4b9ecefcba898624caba9755074b4f5b3
Administrator <a601942905@163.com>
2017-10-28 22:30:57 +0800

    修改服务配置

--- free-framework-admin/src/main/java/com/free/framework/Application.java
+++ free-framework-admin/src/main/java/com/free/framework/Application.java
@@ -1,18 +1,46 @@
 package com.free.framework;
 
+import com.free.framework.plateform.util.web.ApplicationContextUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by Administrator on 2017/6/3.
  */
 @SpringBootApplication
 @Slf4j
-public class Application {
-    public static void main(String[] args) {
-        log.info("======Spring Boot项目启动开始======");
+public class Application implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+
+    /**
+     * 应用名
+     */
+    private static String applicationName;
+
+    /**
+     * 端口
+     */
+    private static Integer port;
+
+    @Override
+    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+        port = event.getEmbeddedServletContainer().getPort();
+        ServerProperties serverProperties = (ServerProperties) ApplicationContextUtils.getBean(ServerProperties.class);
+        applicationName = serverProperties.getDisplayName();
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
         SpringApplication.run(Application.class, args);
-        log.info("======Spring Boot项目启动结束======");
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        log.info("=================================");
+        log.info("      项目名称:" + applicationName);
+        log.info("      项目访问地址:" + ip + ":" + port);
+        log.info("=================================");
     }
 }
