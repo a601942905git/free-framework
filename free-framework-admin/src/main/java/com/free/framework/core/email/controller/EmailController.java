083174f4955ee1c8774d304584747168a26aa4db
Administrator <a601942905@163.com>
2017-11-06 23:37:32 +0800

    添加邮件功能

--- free-framework-admin/src/main/java/com/free/framework/core/email/controller/EmailController.java
+++ free-framework-admin/src/main/java/com/free/framework/core/email/controller/EmailController.java
@@ -1,9 +1,13 @@
 package com.free.framework.core.email.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * com.free.framework.core.email.EmailController
@@ -11,16 +15,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author lipeng
  * @dateTime 2017/11/6 22:53
  */
+@Slf4j
 @Controller
 @RequestMapping(EmailControllerMappingUrl.EMAIL_CONTROLLER)
 public class EmailController {
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
     @GetMapping(EmailControllerMappingUrl.EMAIL)
+    @ResponseBody
     public void sendEmail() {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("601942905@qq.com");
-        simpleMailMessage.setTo("a601942905@163.com");
+        simpleMailMessage.setFrom("a601942905@163.com");
+        simpleMailMessage.setTo("601942905@qq.com");
         simpleMailMessage.setSubject("主题:简单邮件");
         simpleMailMessage.setText("测试邮件发送功能");
+        try {
+            javaMailSender.send(simpleMailMessage);
+            log.info("邮件发送成功");
+        } catch (Exception e) {
+            log.info("邮件发送失败:", e);
+        }
     }
 }
