71d0b51675175f8933e5022bc089d9fb40f27f19
lipeng <a601942905>
2017-11-16 14:30:25 +0800

    去除多余的注释

--- free-framework-admin/src/main/java/com/free/framework/plateform/config/druid/datasource/DruidDataSourceConfig.java
+++ free-framework-admin/src/main/java/com/free/framework/plateform/config/druid/datasource/DruidDataSourceConfig.java
@@ -74,8 +74,8 @@ public class DruidDataSourceConfig {
     @Value("{spring.datasource.connectionProperties}")
     private String connectionProperties;
 
-    @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+    @Bean
+    @Primary
     public DataSource dataSource(){
         DruidDataSource datasource = new DruidDataSource();
 
