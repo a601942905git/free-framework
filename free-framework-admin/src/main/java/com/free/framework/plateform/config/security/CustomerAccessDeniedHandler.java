d6feab81e836c754986dbec7965bc5b7d9969d4a
lipeng <a601942905>
2017-12-08 18:03:38 +0800

    集成spring security

---
+++ free-framework-admin/src/main/java/com/free/framework/plateform/config/security/CustomerAccessDeniedHandler.java
@@ -0,0 +1,38 @@
+package com.free.framework.plateform.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author smile
+ */
+@Slf4j
+@Component
+public class CustomerAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException {
+
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            log.info("User '" + auth.getName()
+                    + "' attempted to access the protected URL: "
+                    + httpServletRequest.getRequestURI());
+        }
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/403");
+
+    }
+}
---
+++ free-framework-admin/src/main/java/com/free/framework/plateform/config/security/SpringSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.free.framework.plateform.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+/**
+ * spring security配置
+ * @author smile
+ */
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/login/login").permitAll()
+                    .antMatchers("/users/**").hasAnyRole("USER")
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll()
+                    .and()
+                    .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
+}
