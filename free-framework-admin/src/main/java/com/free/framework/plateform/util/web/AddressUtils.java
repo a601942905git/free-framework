460a11474a21e134e5293342ee8a4e2e049e6673
lipeng <a601942905>
2018-01-30 14:54:30 +0800

    修改代码规范

--- free-framework-admin/src/main/java/com/free/framework/plateform/util/web/AddressUtils.java
+++ free-framework-admin/src/main/java/com/free/framework/plateform/util/web/AddressUtils.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class AddressUtils {
 
+    public static final String UNKNOWN_ADDRESS = "unknown";
+
     /**
      * 获取请求ip地址
      * @return
@@ -17,19 +19,19 @@ public class AddressUtils {
     public static String getIpAddress() {
         HttpServletRequest request = WebContextUtils.getRequest();
         String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN_ADDRESS.equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN_ADDRESS.equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN_ADDRESS.equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN_ADDRESS.equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN_ADDRESS.equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
         return ip;
