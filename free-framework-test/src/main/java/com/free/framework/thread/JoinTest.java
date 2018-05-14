3fd767a861c50a1ac5178eab89e07a15174cef54
lipeng <a601942905@163.com>
2018-05-14 14:29:52 +0800

    并发CyclicBarrier和Semaphore的学习

--- free-framework-test/src/main/java/com/free/framework/thread/JoinTest.java
+++ free-framework-test/src/main/java/com/free/framework/thread/JoinTest.java
@@ -9,17 +9,27 @@ package com.free.framework.thread;
 public class JoinTest {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                System.out.println("线程1：" + i);
-            }
-        });
+        Thread thread = null;
+        for (int i = 0; i < 10; i++) {
+            thread = new Thread(new Player((i + 1) + "号玩家"));
+            thread.start();
+            thread.join();
+        }
+        System.out.println("进入游戏");
+    }
 
-        /**
-         * 调用join()方法的线程能够在此之前执行完毕
-         * 类似阻塞，直到调用该方法的的线程执行完毕
-         */
-        thread.start();
-        System.out.println("主线程。。。。。。");
+    static class Player implements Runnable {
+
+        private String name;
+
+        Player(String name) {
+            this.name = name;
+        }
+
+        @Override
+
+        public void run() {
+            System.out.println(name + "已经加载完毕，等待其它玩家加载！");
+        }
     }
 }
---
+++ free-framework-test/src/main/java/com/free/framework/thread/SemaphoreTest.java
@@ -0,0 +1,48 @@
+package com.free.framework.thread;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * com.free.framework.thread.SemaphoreTest
+ *
+ * @author lipeng
+ * @dateTime 2018/5/14 上午11:33
+ */
+public class SemaphoreTest {
+
+    public static void main(String[] args) {
+        // 8个工人
+        Integer workerNumber = 8;
+        // 5台机器
+        Semaphore semaphore = new Semaphore(5);
+        for (int i = 0; i < workerNumber; i++) {
+            new Thread(new Worker((i + 1), semaphore)).start();
+        }
+    }
+
+    static class Worker implements Runnable{
+
+        private Integer number;
+
+        private Semaphore semaphore;
+
+        Worker(Integer number, Semaphore semaphore) {
+            this.number = number;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+                System.out.println(number + "号工人占用一台机器");
+                // 模拟工人使用机器的时间
+                Thread.sleep(number * 1000);
+                semaphore.release();
+                System.out.println(number + "号工人释放一台机器");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
